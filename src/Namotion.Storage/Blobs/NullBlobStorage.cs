faca22b43cc7c5390905bfb20b63f04a56a91aea
Rico Suter <mail@rsuter.com>
2020-05-15 09:20:36 +0200

    Add null implementations

---
+++ src/Namotion.Storage/Blobs/NullBlobStorage.cs
@@ -0,0 +1,49 @@
+﻿using Namotion.Storage.Abstractions;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Namotion.Storage
+{
+    public class NullBlobStorage : IBlobStorage
+    {
+        public Task DeleteAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> ExistsAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<BlobElement> GetAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult<BlobElement>(null);
+        }
+
+        public Task<BlobElement[]> ListAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(new BlobElement[0]);
+        }
+
+        public Task<Stream> OpenAppendAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Stream.Null);
+        }
+
+        public Task<Stream> OpenReadAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Stream.Null);
+        }
+
+        public Task<Stream> OpenWriteAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Stream.Null);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
