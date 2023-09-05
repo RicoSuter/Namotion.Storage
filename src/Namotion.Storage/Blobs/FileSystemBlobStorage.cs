79a61530f2d7525d95d5c091678266f77338acf8
Rico Suter <rico.suter@buhlergroup.com>
2023-09-05 18:11:42 +0200

    Add path replacements

--- src/Namotion.Storage/Blobs/FileSystemBlobStorage.cs
+++ src/Namotion.Storage/Blobs/FileSystemBlobStorage.cs
@@ -12,28 +12,38 @@ namespace Namotion.Storage
     public class FileSystemBlobStorage : IBlobStorage, IBlobContainer
     {
         private readonly string _basePath;
+        private readonly IReadOnlyDictionary<string, string> _pathReplacements;
 
-        private FileSystemBlobStorage(string basePath)
+        /// <summary>
+        /// Creates a new instance of the file system based blob storage.
+        /// </summary>
+        /// <param name="basePath">The base path.</param>
+        /// <param name="pathReplacements">The path replacements.</param>
+        protected FileSystemBlobStorage(string basePath, IReadOnlyDictionary<string, string> pathReplacements = null)
         {
             _basePath = basePath;
+            _pathReplacements = pathReplacements ?? new Dictionary<string, string> { { ":", "%3A" } };
         }
 
         /// <summary>
         /// Creates a new storage with an abosulte base path.
         /// </summary>
         /// <param name="basePath">The absolute base path.</param>
+        /// <param name="pathReplacements">The path replacements or use defaults (':' => '%3A').</param>
         /// <returns>The blob storage or container.</returns>
-        public static FileSystemBlobStorage CreateWithBasePath(string basePath)
+        public static FileSystemBlobStorage CreateWithBasePath(string basePath, IReadOnlyDictionary<string, string> pathReplacements = null)
         {
-            return new FileSystemBlobStorage(basePath);
+            return new FileSystemBlobStorage(basePath, pathReplacements);
         }
 
+        /// <inheritdoc />>
         public Task<bool> ExistsAsync(string path, CancellationToken cancellationToken = default)
         {
             var fullPath = GetFullPath(path);
             return Task.FromResult(File.Exists(fullPath));
         }
 
+        /// <inheritdoc />>
         public Task<BlobElement> GetAsync(string path, CancellationToken cancellationToken)
         {
             try
@@ -55,11 +65,13 @@ namespace Namotion.Storage
             }
         }
 
+        /// <inheritdoc />>
         public Task UpdateMetadataAsync(string path, IDictionary<string, string> metadata, CancellationToken cancellationToken = default)
         {
             throw new System.NotImplementedException();
         }
 
+        /// <inheritdoc />>
         public Task<Stream> OpenReadAsync(string path, CancellationToken cancellationToken = default)
         {
             try
@@ -77,6 +89,7 @@ namespace Namotion.Storage
             }
         }
 
+        /// <inheritdoc />>
         public Task<Stream> OpenAppendAsync(string path, CancellationToken cancellationToken = default)
         {
             try
@@ -100,6 +113,7 @@ namespace Namotion.Storage
             }
         }
 
+        /// <inheritdoc />>
         public Task<Stream> OpenWriteAsync(string path, CancellationToken cancellationToken = default)
         {
             var fullPath = GetFullPath(path);
@@ -112,6 +126,7 @@ namespace Namotion.Storage
             return Task.FromResult<Stream>(File.Open(fullPath, FileMode.Create, FileAccess.Write));
         }
 
+        /// <inheritdoc />>
         public Task DeleteAsync(string path, CancellationToken cancellationToken = default)
         {
             var fullPath = GetFullPath(path);
@@ -119,6 +134,7 @@ namespace Namotion.Storage
             return Task.CompletedTask;
         }
 
+        /// <inheritdoc />>
         public Task<BlobElement[]> ListAsync(string path, CancellationToken cancellationToken = default)
         {
             try
@@ -139,12 +155,23 @@ namespace Namotion.Storage
             }
         }
 
+        /// <inheritdoc />>
         public void Dispose()
         {
         }
 
+        /// <summary>
+        /// Gets the full path based on the identifier.
+        /// </summary>
+        /// <param name="identifier">The item identifier.</param>
+        /// <returns>The full path.</returns>
         protected virtual string GetFullPath(string identifier)
         {
+            foreach (var pathReplacement in _pathReplacements)
+            {
+                identifier = identifier.Replace(pathReplacement.Key, pathReplacement.Value);
+            }
+
             return Path.Combine(_basePath, identifier);
         }
     }
